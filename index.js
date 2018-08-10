f4ed1419020174b734e8b8c26f87e6ae23a20088
Tim Abrahamsen <timmy_neutron@mac.com>
2018-08-10 16:37:39 -0400

    Add error handler

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express()
 
 app.use('/', routes);
 
+app.use((err, req, res, next) => {
+	res.status(err.status || 500).send(err.message)
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
