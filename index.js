001cef213e7717f9f49013fcd08bebb601be4d7d
Tim Abrahamsen <timmy_neutron@mac.com>
2018-08-10 15:37:53 -0400

    Move routes to different folder

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
-app.get('/', (req, res) => {
-	res.status(200).send('Hello world!')
-})
+app.use('/', routes);
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
